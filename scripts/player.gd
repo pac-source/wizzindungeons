eb9de84646ce3851ff98a46c540a69b3613ee79b
Rukesh <pac-source@github.com>
2025-08-19 15:29:53 +0530

    updated player physics

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,20 @@
 extends CharacterBody2D
 
 const SPEED = 235.0
-const ACCELERATION = 210.0
+const ACCELERATION = 150.0
 const FRICTION = 240.0
 
 const GRAVITY = 2000.0
 const FALL_GRAVITY = 3000.0
-const FAST_FALL_GRAVITY = 5000.0
+const FAST_FALL_GRAVITY = 30000.0
 
 const JUMP_VELOCITY = -500.0
 
 const INPUT_BUFFER = 0.125
-const JUMP_BUFFER = 0.18
+const COYOTE_TIME = 0.08 
 
 var input_buffer_timer : Timer
-var jump_buffer_timer : Timer
+var coyote_timer = 0.0
 var jump_available : bool = true
 
 @onready var sprite_2d := $AnimatedSprite2D
@@ -26,17 +26,12 @@ func _ready():
 	input_buffer_timer.one_shot = true
 	add_child(input_buffer_timer)
 
-	# Setup coyote timer
-	jump_buffer_timer = Timer.new()
-	jump_buffer_timer.wait_time = JUMP_BUFFER
-	jump_buffer_timer.one_shot = true
-	add_child(jump_buffer_timer)
-
-func _physics_process(delta):
+func _physics_process(delta : float):
 	var horizontal_input = Input.get_axis("ui_left","ui_right") 
 	var jump_attempted = Input.is_action_just_pressed("ui_select")
 	
-	# Sprite animation
+	## SPRITE  ANIMATION
+	# basic animation
 	if is_on_floor():
 		if horizontal_input:
 			sprite_2d.play("run")
@@ -45,12 +40,14 @@ func _physics_process(delta):
 	else:
 		sprite_2d.play("jump")
 	
+	#flip sprite
 	if horizontal_input == -1:
 		sprite_2d.flip_h = true
 	if horizontal_input == 1:
 		sprite_2d.flip_h = false
 		
-	# Handle jumping
+	## HANDLE JUMPING	
+	# buffer jumping
 	if jump_attempted or input_buffer_timer.time_left > 0:
 		if jump_available:
 			velocity.y = JUMP_VELOCITY
@@ -58,31 +55,40 @@ func _physics_process(delta):
 		elif jump_attempted:
 			input_buffer_timer.start()
 	
-	if Input.is_action_just_released("ui_select") and velocity.y < 0:
-		velocity.y = JUMP_VELOCITY / 4
-
+	if input_buffer_timer.timeout:
+		jump_available = false
 	if is_on_floor():
 		jump_available = true
-		jump_buffer_timer.stop()
-	else:
-		if jump_available:
-			if jump_buffer_timer.is_stopped():
-				jump_buffer_timer.start()
-		velocity.y += get_the_gravity() * delta
 		
-		if jump_buffer_timer.timeout:
-				jump_available = false
-
+	# coyote jump
+		coyote_timer = COYOTE_TIME
+	coyote_timer -= delta
+	# print("coyote time: " + str(coyote_timer) + "\ndelta: " + str(delta))
+	if coyote_timer <= 0:
+		jump_available = false
+	else:	
+		jump_available = true
+	
+	# short jump	
+	if Input.is_action_just_released("ui_select") and velocity.y < 0:
+		velocity.y = JUMP_VELOCITY / 4
+	
+	## MOVEMENT
+	# dashing
 	var floor_damping : float = 1.0 if is_on_floor() else 0.2
 	var dash_multiplier : float = 1.6 if Input.is_action_pressed("ui_home") else 1.0
-
+	
+	#basic movements
 	if horizontal_input:
 		velocity.x = move_toward(velocity.x, horizontal_input * SPEED * dash_multiplier, ACCELERATION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * dash_multiplier * delta)
-
+		velocity.x = move_toward(velocity.x, 0, FRICTION * floor_damping * delta)
+	
+	# velocity.x = horizontal_input * SPEED * dash_multiplier * delta
+	
+	## GRAVITY AND OTHER PHYSICS
+	velocity.y += get_the_gravity() * delta	
 	move_and_slide()
-
 
 func get_the_gravity():
 	if Input.is_action_just_pressed("ui_down"):
