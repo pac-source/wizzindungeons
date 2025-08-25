5a3f46b6b34b4f1bde03c9400f6102f148f16236
Rukesh <193492872+pac-source@users.noreply.github.com>
2025-08-25 10:20:57 +0530

    coin counter

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,6 @@
-extends StaticBody2D
-
+extends Node2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	player_variables.coins_collected += 1
+	#print(player_variables.coins_collected)
 	queue_free()
