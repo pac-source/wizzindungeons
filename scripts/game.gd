9523c8e2ae3af3e53368ec7a7ccf03e762125348
Rukesh <pac-source@github.com>
2025-08-25 12:42:25 +0530

    updated coin counter

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
 
-func _on_death_zone_body_entered(body: Node2D) -> void:
+func _on_death_zone_area_entered(area: Area2D) -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	player_variables.coins_collected = 0
